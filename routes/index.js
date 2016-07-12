47736fc21631773d452c24c48fee2e0f2f0205a9
Darwin Yang <darwin.l.yang@gmail.com>
2016-07-11 22:40:27 -0400

    Testing slacking post getting hit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log("Index has been hit.");
   res.render('index', { title: 'Express' });
 });
+
+router.post('/test', function(req, res) {
+	console.log("Post has been hit.");
+});
+
+
+// router.post('/adduser', function(req, res) {
+//     var db = req.db;
+//     var collection = db.get('userlist');
+//     collection.insert(req.body, function(err, result){
+//         res.send(
+//             (err === null) ? { msg: '' } : { msg: err }
+//         );
+//     });
+// });
+
+
+
 
 module.exports = router;
